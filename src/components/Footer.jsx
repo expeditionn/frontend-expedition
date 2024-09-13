9b9462abb8af23ccbbde34cef5f9f40b8d4cc3cc
Yashi Gupta <yashi.g23csai@nst.rishihood.edu.in>
2024-09-13 10:11:37 +0530

    footer designs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 import { FaRegCopyright } from "react-icons/fa";
 import { GiFallingStar } from "react-icons/gi";
 import { MdAirplanemodeActive } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { MdOutlineMailOutline } from "react-icons/md";
 import styles from '../components-css/footer.module.css';
 function Footer() {
   return (
@@ -11,23 +14,25 @@ function Footer() {
         <button className = {styles.signUpBtn}>Create your account <GiFallingStar className = {styles.signupIcon}/></button>
       </div>
       <hr className = {styles.hRule} />
-      <div>
+      <div className = {styles.body2}>
         <div className = {styles.logoDiv}>
           <MdAirplanemodeActive className = {styles.mainIcon} />
           <p className = {styles.brandName}>EXPEDIT<span className = {styles.onText}>ION</span></p>
         </div>
-        <div>
-          <p></p>
-          <p></p>
-          <p></p>
+        <div className = {styles.footerTextBody}>
+          <p className = {styles.footerText}>Home</p>
+          <p className = {styles.footerText}>Places</p>
+          <p className = {styles.footerText}>Tickets</p>
         </div>
-        <div>
-          <p>Home</p>
-          <p>Places</p>
-          <p>Tickets</p>
+        <div className = {styles.iconBody}>
+          <p><FaGithub /></p>
+          <p><FaLinkedinIn /></p>
+          <p><MdOutlineMailOutline /></p>
         </div>
       </div>
-      <p><FaRegCopyright /> 2024 Expedition, Inc. All rights reserved.</p>
+      <div className = {styles.copyRightBody}>
+        <p className = {styles.allRights}><FaRegCopyright className = {styles.copyRightIcon} /> 2024 Expedition, Inc. All rights reserved.</p>
+      </div>
     </div>
   )
 }
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,17 @@
+function SignUpForm() {
+  return (
+    <div>
+      <p>Just one last step and you're in!</p>
+      <p>Enter your details below to create an account</p>
+      <form>
+        <div>
+          <p>Full Name*</p>
+          <input placeholder = "" />
+        </div>
+        
+      </form>
+    </div>
+  )
+}
+
+export default SignUpForm;
